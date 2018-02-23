686d3afa0979af0c836b4f23797b9cabc92565e0
Noctaum <Noctaum@users.noreply.github.com>
2018-02-23 21:21:57 +0300

    Task 7 advZero answer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  
+   var pseudoBase, a = 0,c,pseudoNum, psNum, z;
+    pseudoNum = number;
+    pseudoBase = base;
+    for(let i = 2; i <= base; i++){
+      if(pseudoBase % i == 0){
+        a = 0;
+        while(pseudoBase % i == 0){
+          a++;
+          pseudoBase = Math.floor(pseudoBase/i);
+      }
+      z = 0;
+        psNum = number;
+        while (psNum/i > 0){
+          z+=Math.floor(psNum/i);
+          psNum = Math.floor(psNum/i);
+      }
+      if(pseudoNum > z/a){ 
+        pseudoNum=z/a;
+      } 
+    }
+  }
+  return Math.floor(pseudoNum);
 }
